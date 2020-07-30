e6840fbd5a122d60b2ecc31772aa2a74de930c3c
çağlar özcan <cglrozcan@gmail.com>
2020-07-30 06:53:11 +0300

    Add files via upload

---
+++ FileNameCreator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Library.Helpers
+{
+	public class FileNameCreator
+	{
+		public string Create(string name, string path)
+		{
+			int counter = 1;
+			string newName = name, extension = "", tempName = "";
+
+			string[] splitName = name.Split('.');
+
+			extension = splitName.Last();
+
+			tempName = Slug(String.Join("", splitName.Take(splitName.Length - 1)));
+
+			newName = tempName;
+
+			while (System.IO.File.Exists(path + newName + "." + extension))
+			{
+				newName = tempName + "-" + (counter++).ToString();
+			}
+
+			return newName + "." + extension;
+		}
+	}
+}
